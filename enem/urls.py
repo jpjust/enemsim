39ae831e5190b5f26da9f8771aae14d8e742b077
João Paulo Just Peixoto <just1982@gmail.com>
2022-04-16 19:24:01 -0300

    feat: implement authentication

--- enem/urls.py
+++ enem/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',            views.index,        name='index'),
+    path('login',       views.login_user,   name='login'),
+    path('logout',      views.logout_user,  name='logout'),
+    path('signup',      views.signup,       name='signup'),
+    path('dashboard',   views.dashboard,    name='dashboard')
 ]
--- enem/views.py
+++ enem/views.py
@@ -1,6 +1,91 @@
-from django.shortcuts import render
+from MySQLdb import IntegrityError
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Root view
 def index(request):
-    return HttpResponse("Hello, world. You're at the Enem index.")
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    return redirect('login')
+
+# Log in
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    
+    if request.method == 'POST':
+        data = request.POST
+
+        # Authenticate user
+        user = authenticate(request, username=data['email'], password=data['pwd'])
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        
+        else:
+            return render(request, 'login.html', {
+                'data': data,
+                'error_message': 'Erro de autenticação.'
+            })
+
+# Log out
+@login_required
+def logout_user(request):
+    logout(request)
+    return redirect('index')
+
+# Sign up
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    
+    if request.method == 'POST':
+        data = request.POST
+
+        # Check all fields
+        if not data['first_name'] or not data['last_name'] or not data['email'] \
+           or not data['school'] or not data['pwd1'] or not data['pwd2']:
+            return render(request, 'signup.html', {
+                'data': data,
+                'error_message': 'Todos os campos precisam ser preenchidos.'
+            })
+        
+        # Check passwords
+        if data['pwd1'] != data['pwd2']:
+            return render(request, 'signup.html', {
+                'data': data,
+                'error_message': 'As senhas digitadas não coincidem.'
+            })
+        
+        # Form data is OK
+        try:
+            user = User.objects.create_user(
+                username=data['email'],
+                email=data['email'],
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                password=data['pwd1']
+            )
+            user.save()
+        except IntegrityError:
+            return render(request, 'signup.html', {
+                'data': data,
+                'error_message': 'O e-mail fornecido já está cadastrado.'
+            })
+
+        return render(request, 'login.html', {
+            'info_message': 'Cadastro efetuado com sucesso!'
+        })
+
+# Dashboard
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
