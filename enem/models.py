a19d3e1e113477916e5c48eda3d7ba0c11c6f435
João Paulo Just Peixoto <just1982@gmail.com>
2022-04-16 17:01:31 -0300

    Initial model and database setup.

--- enem/models.py
+++ enem/models.py
@@ -1,3 +1,89 @@
+import pickletools
 from django.db import models
 
-# Create your models here.
+# Timestamps
+class TimeStampMixin(models.Model):
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+# Roles
+class Role(TimeStampMixin):
+    name            = models.CharField(max_length=10)
+    description     = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+# Users
+class User(TimeStampMixin):
+    role            = models.ForeignKey(Role, on_delete=models.CASCADE)
+    email           = models.CharField(max_length=100, unique=True)
+    password        = models.CharField(max_length=100)
+    fullname        = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.fullname
+
+# Schools
+class School(TimeStampMixin):
+    name            = models.CharField(max_length=100)
+
+# Professors and teachers
+class Professor(TimeStampMixin):
+    user            = models.ForeignKey(User, on_delete=models.CASCADE)
+    school          = models.ForeignKey(School, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.fullname
+
+# Students
+class Student(TimeStampMixin):
+    user            = models.ForeignKey(User, on_delete=models.CASCADE)
+    school          = models.ForeignKey(School, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.fullname
+
+# Exams
+class Exam(TimeStampMixin):
+    student         = models.ForeignKey(Student, on_delete=models.CASCADE)
+    score           = models.IntegerField
+
+# Topics
+class Topic(TimeStampMixin):
+    text            = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.text
+
+# Subjects
+class Subject(TimeStampMixin):
+    topic           = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text            = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.text
+
+# Questions
+class Question(TimeStampMixin):
+    professor       = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    subject         = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    text            = models.CharField(max_length=1000)
+    occurences      = models.IntegerField
+    corrects        = models.IntegerField
+
+    def __str__(self):
+        return self.text
+
+# Alternatives
+class Alternative(TimeStampMixin):
+    question        = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text            = models.CharField(max_length=200)
+    correct         = models.BooleanField
+    picks           = models.IntegerField
+
+    def __str__(self):
+        return self.text
--- enemsim/settings.py
+++ enemsim/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'enem.apps.EnemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'enemsim.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'NAME': 'enem',
+        'USER': 'enem',
+        'PASSWORD': 'enem'
     }
 }
 
