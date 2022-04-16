7db37c2a5d36c57378981d8edb3e456314a58d33
João Paulo Just Peixoto <just1982@gmail.com>
2022-04-16 17:08:40 -0300

    feat: register models in admin site

--- enem/admin.py
+++ enem/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(School)
+admin.site.register(Professor)
+admin.site.register(Student)
+admin.site.register(Topic)
+admin.site.register(Subject)
